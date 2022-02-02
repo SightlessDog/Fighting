1f11af3db85f2411cf352d916ce0f743ad149685
SophieMitPh <SophieMitPh>
2022-02-02 14:54:50 +0100

    added settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -11,6 +11,28 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField] private GameObject confirmationPrompt = null;
     [SerializeField] private float _defaultVolume = 0.5f;
 
+	[SerializeField] private TMP_Dropdown qualityDropdown;
+	[SerializeField] private Toggle fullScreenToggle;
+
+    private int _qualityLevel;
+    private bool _isFullScreen;
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    
+    public void SetFullScreen(bool isFullScreen)
+    {
+		if ( _isFullScreen ) {
+			Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+			_isFullScreen = false;
+	} 
+		else { 
+			Screen.fullScreenMode = FullScreenMode.Windowed;
+			_isFullScreen = true;
+		}    
+	}
 
     public void SetVolume(float volume)
     {
@@ -18,21 +40,34 @@ public class SettingsMenu : MonoBehaviour
         _volumeText.text = volume.ToString("0.0");
     }
 
-    public void SaveVolume()
+    public void SaveSettings()
     {
         PlayerPrefs.SetFloat("volume", AudioListener.volume);
+		PlayerPrefs.SetFloat("volume", _volumeSlider.value);
+                
+        PlayerPrefs.SetInt("quality", _qualityLevel);
+
+        PlayerPrefs.SetInt("fullscreen", (_isFullScreen ? 1 : 0));
+
         StartCoroutine(ConfirmationBox());
     }
     
-    public void ResetVolume()
+    public void ResetSettings()
     {
         AudioListener.volume = _defaultVolume;
         _volumeSlider.value = _defaultVolume;
         _volumeText.text = _defaultVolume.ToString("0.0");
+		
+		qualityDropdown.value = 1;
+		QualitySettings.SetQualityLevel(1);
+		
+        fullScreenToggle.isOn = true;
+		Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
     }
 
     public IEnumerator ConfirmationBox()
     {
+		Debug.Log("CONFIRMATION!");
         confirmationPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
         confirmationPrompt.SetActive(false);
